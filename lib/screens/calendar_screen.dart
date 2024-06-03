867e379876930c138e0304e2c65c58aeb4502c37
NVSTT <boulevarduhapolg@gmail.com>
2024-06-04 01:02:01 +0700

    modified:   lib/screens/calendar_screen.dart 	deleted:    lib/screens/checklist_detail.dart 	modified:   lib/screens/checklist_screen.dart 	modified:   lib/sqflite/db_helper.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -1,15 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:flutter_application_1/sqflite/db_helper.dart';
 
-class CalendarScreen extends StatelessWidget {
+class CalendarScreen extends StatefulWidget {
+  @override
+  _CalendarScreenState createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  late final Map<DateTime, List<dynamic>> _events;
+  late final ValueNotifier<List<dynamic>> _selectedEvents;
+  late final TextEditingController _eventController;
+  late final CalendarFormat _calendarFormat;
+  late final DateTime _focusedDay;
+  late final DateTime _selectedDay;
+  late List<Map<String, dynamic>> _checklists;
+
+  _CalendarScreenState() {
+    _checklists = [];
+  }
+
+ @override
+void initState() {
+  super.initState();
+  _events = {}; // Список событий по датам
+  _selectedEvents = ValueNotifier(_getEventsForDay(DateTime.now()));
+  _eventController = TextEditingController();
+  _calendarFormat = CalendarFormat.month;
+  _focusedDay = DateTime.now();
+  _selectedDay = DateTime.now();
+  _loadChecklists();
+}
+
+Future<void> _loadChecklists() async {
+  try {
+    final List<Map<String, dynamic>> checklists = await DBHelper.getChecklists();
+    print('Loaded checklists: $checklists');
+    setState(() {
+      _checklists = checklists;
+    });
+  } catch (e) {
+    print('Failed to load checklists: $e');
+  }
+}
+
+
+  List<dynamic> _getEventsForDay(DateTime day) {
+    final List<dynamic> events = [];
+    for (final checklist in _checklists) {
+      final DateTime checklistDate = DateTime.parse(checklist['id']);
+      if (isSameDay(checklistDate, day) && checklist['isCompleted'] == 0) {
+        events.add(checklist);
+      }
+    }
+    return events;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Календарный план'),
-      ),
-      body: Center(
-        child: Text('Календарный план на Март 2024'),
+            title: Container(
+              color: Colors.grey[800], // серый фон
+              padding: EdgeInsets.all(8.0), 
+              child: Center(
+              child: Text('Календарный план',
+              style: TextStyle(color: Colors.white), // белый текст
+              ),
+            ),
+          ),
+          backgroundColor: Colors.grey[800], // делаем AppBar темно-серым
+          elevation: 0, // убираем тень
+        ),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: DateTime.utc(2024, 3, 1),
+            lastDay: DateTime.utc(2024, 3, 31),
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+                _selectedEvents.value = _getEventsForDay(selectedDay);
+              });
+            },
+            eventLoader: _getEventsForDay,
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _selectedEvents.value.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_selectedEvents.value[index].toString()),
+                  // Здесь можно отобразить другие детали задачи
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _selectedEvents.dispose();
+    _eventController.dispose();
+    super.dispose();
   }
 }
--- lib/screens/checklist_screen.dart
+++ lib/screens/checklist_screen.dart
@@ -1,6 +1,7 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/sqflite/db_helper.dart';
-import './checklist_detail.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ChecklistScreen extends StatefulWidget {
   @override
@@ -8,144 +9,321 @@ class ChecklistScreen extends StatefulWidget {
 }
 
 class _ChecklistScreenState extends State<ChecklistScreen> {
-  final _formKey = GlobalKey<FormState>();
-  String checklistTitle = '';
-  late Future<List<Map<String, dynamic>>> _checklistFuture;
+  late TextEditingController _titleController;
+  late String? _photoPath;
 
   @override
   void initState() {
     super.initState();
-    _loadChecklists();
+    _titleController = TextEditingController();
+    _photoPath = null;
   }
 
-  void _loadChecklists() {
-    setState(() {
-      _checklistFuture = DBHelper.getChecklists();
-    });
+  Future<void> _pickImage() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        _photoPath = image.path;
+      });
+    }
   }
 
-  void _openChecklist(String id) {
-    Navigator.of(context)
-        .push(MaterialPageRoute(
-          builder: (context) => ChecklistDetailScreen(
-            id,
-            _formKey,
-            checklistTitle,
-            DBHelper.getChecklistById(id),
-          ),
-        ))
-        .then((_) => _loadChecklists()); // обновляем список после возвращения с экрана деталей
+  Future<void> _addChecklist() async {
+    if (_titleController.text.isNotEmpty) {
+      await DBHelper.addChecklist({
+        'id': DateTime.now().toString(),
+        'title': _titleController.text,
+        'isCompleted': 0,
+        'photo': _photoPath ?? '',
+      });
+      _titleController.clear();
+      setState(() {
+        _photoPath = null;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Container(
-          color: Colors.grey[800],
-          padding: EdgeInsets.all(8.0),
-          child: Center(
-            child: Text(
-              'Чек-лист',
-              style: TextStyle(color: Colors.white),
+            title: Container(
+              color: Colors.grey[800], // серый фон
+              padding: EdgeInsets.all(8.0), 
+              child: Center(
+              child: Text('Чек-лист',
+                style: TextStyle(color: Colors.white), // белый текст
+              ),
             ),
           ),
+          backgroundColor: Colors.grey[800], // делаем AppBar темно-серым
+          elevation: 0, // убираем тень
         ),
-        backgroundColor: Colors.grey[800],
-        elevation: 0,
-      ),
-      body: FutureBuilder(
-        future: _checklistFuture,
-        builder: (ctx, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.error != null) {
-            return Center(child: Text('Произошла ошибка!'));
-          } else {
-            return ListView.builder(
-              itemCount: snapshot.data?.length,
-              itemBuilder: (ctx, index) {
-                return ListTile(
-                  title: Text(snapshot.data?[index]['title']),
-                  subtitle: Text('Статус: ' +
-                      (snapshot.data?[index]['isCompleted'] == 1
-                          ? 'Выполнено'
-                          : 'В процессе')),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      IconButton(
-                        icon: Icon(Icons.edit),
-                        onPressed: () => _openChecklist(snapshot.data?[index]['id']),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          DBHelper.deleteChecklist(snapshot.data?[index]['id']);
-                          _loadChecklists();
-                        },
-                      ),
-                    ],
-                  ),
-                  onTap: () => _openChecklist(snapshot.data?[index]['id']),
-                );
-              },
-            );
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: Text('Новый чек-лист'),
-                content: Form(
-                  key: _formKey,
-                  child: TextFormField(
-                    onChanged: (value) {
-                      checklistTitle = value;
-                    },
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Пожалуйста, введите название';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'Название',
-                    ),
-                  ),
-                ),
-                actions: <Widget>[
-                  TextButton(
-                    child: Text('Отмена'),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                  TextButton(
-                    child: Text('Сохранить'),
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        DBHelper.addChecklist({
-                          'id': DateTime.now().toString(),
-                          'title': checklistTitle,
-                          'isCompleted': 0,
-                        });
-                        _loadChecklists();
-                        Navigator.of(context).pop();
-                      }
-                    },
-                  ),
-                ],
-              );
-            },
-          );
-        },
-        child: Icon(Icons.add),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                labelText: 'Название чек-листа',
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _pickImage(),
+              child: Text('Выбрать фото'),
+            ),
+            SizedBox(height: 20),
+            _photoPath == null
+                ? Text('Фотография не выбрана.')
+                : Image.file(File(_photoPath!)),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _addChecklist(),
+              child: Text('Добавить чек-лист'),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: FutureBuilder<List<Map<String, dynamic>>>(
+                future: DBHelper.getChecklists(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Произошла ошибка!'));
+                  } else {
+                    return ListView.builder(
+                      itemCount: snapshot.data?.length,
+                      itemBuilder: (ctx, index) {
+                        var checklist = snapshot.data![index];
+                        return ListTile(
+                          title: Text(checklist['title']),
+                          subtitle: Text('Статус: ' +
+                              (checklist['isCompleted'] == 1
+                                  ? 'Выполнено'
+                                  : 'В процессе')),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ChecklistDetailScreen(checklist),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+class ChecklistDetailScreen extends StatefulWidget {
+  final Map<String, dynamic> checklist;
+
+  ChecklistDetailScreen(this.checklist);
+
+  @override
+  _ChecklistDetailScreenState createState() => _ChecklistDetailScreenState();
+}
+
+class _ChecklistDetailScreenState extends State<ChecklistDetailScreen> {
+  late TextEditingController _taskDescriptionController;
+  late TextEditingController _itemNumberController; // Для номера задачи
+  late File? _confirmationPhoto; // Для фотографии подтверждения
+
+  @override
+  void initState() {
+    super.initState();
+    _taskDescriptionController = TextEditingController();
+    _itemNumberController = TextEditingController();
+    _confirmationPhoto = null;
+  }
+
+  Future<void> _pickConfirmationPhoto() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        _confirmationPhoto = File(image.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+            title: Container(
+              color: Colors.grey[800], // серый фон
+              padding: EdgeInsets.all(8.0), 
+              child: Center(
+              child: Text('Детали чек листа',
+                style: TextStyle(color: Colors.white), // белый текст
+              ),
+            ),
+          ),
+          backgroundColor: Colors.grey[800], // делаем AppBar темно-серым
+          elevation: 0, // убираем тень
+        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Название: ${widget.checklist['title']}',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Статус: ${widget.checklist['isCompleted'] == 1 ? 'Выполнено' : 'В процессе'}',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 20),
+            _buildTaskForm(),
+            SizedBox(height: 20),
+            Expanded(
+              child: _buildTaskList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTaskForm() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          controller: _itemNumberController,
+          keyboardType: TextInputType.number,
+          decoration: InputDecoration(
+            labelText: 'Номер задачи',
+          ),
+        ),
+        SizedBox(height: 10),
+        TextField(
+          controller: _taskDescriptionController,
+          decoration: InputDecoration(
+            labelText: 'Описание задачи',
+          ),
+        ),
+        SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: _pickConfirmationPhoto,
+          child: Text('Выбрать фотографию подтверждения'),
+        ),
+        SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: () {
+            _addOrUpdateTask();
+          },
+          child: Text('Добавить/Обновить задачу'),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTaskList() {
+    return FutureBuilder<List<Map<String, dynamic>>>(
+      future: DBHelper.getChecklistItemsByChecklistId(widget.checklist['id']),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Произошла ошибка при загрузке задач!'));
+        } else {
+          final tasks = snapshot.data ?? [];
+          return ListView.builder(
+            itemCount: tasks.length,
+            itemBuilder: (ctx, index) {
+              final task = tasks[index];
+              return ListTile(
+                title: Text(task['description']),
+                trailing: IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    _editTask(task);
+                  },
+                ),
+                // Здесь можно отобразить другие детали задачи
+              );
+            },
+          );
+        }
+      },
+    );
+  }
+
+  void _addOrUpdateTask() {
+  final description = _taskDescriptionController.text.trim();
+  final itemNumber = int.tryParse(_itemNumberController.text.trim()) ?? 0;
+  final confirmationPhoto = _confirmationPhoto?.path ?? '';
+
+  if (description.isNotEmpty && itemNumber > 0) {
+    final data = {
+      'itemNumber': itemNumber,
+      'description': description,
+      'isCompleted': 0,
+      'confirmationPhoto': confirmationPhoto,
+      'checklistId': widget.checklist['id'],
+    };
+
+    final isUpdate = _itemNumberController.text.isNotEmpty;
+
+    // Конвертация данных в Map<String, Object>
+    final Map<String, Object> dataObject = data.map((key, value) => MapEntry(key, value as Object));
+
+    if (isUpdate) {
+      DBHelper.updateChecklistItem(itemNumber, dataObject).then((_) {
+        setState(() {
+          _taskDescriptionController.clear();
+          _itemNumberController.clear();
+          _confirmationPhoto = null;
+        });
+      }).catchError((error) {
+        print('Ошибка при обновлении задачи: $error');
+        // Обработка ошибок при обновлении задачи
+      });
+    } else {
+      DBHelper.addChecklistItem(dataObject).then((_) {
+        setState(() {
+          _taskDescriptionController.clear();
+          _itemNumberController.clear();
+          _confirmationPhoto = null;
+        });
+      }).catchError((error) {
+        print('Ошибка при добавлении задачи: $error');
+        // Обработка ошибок при добавлении задачи
+      });
+    }
+  }
+}
+
+
+
+  void _editTask(Map<String, dynamic> task) {
+    _itemNumberController.text = task['itemNumber'].toString();
+    _taskDescriptionController.text = task['description'];
+    // You can set the confirmation photo here if it exists
+    // _confirmationPhoto = File(task['confirmationPhoto']);
+    // Then update the UI to reflect the editing mode
+    setState(() {});
+  }
+}
+
+
+
+
+
